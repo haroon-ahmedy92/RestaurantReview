0ade1611d407f7aa56579953508cb64d8de087ee
haroon-ahmedy92 <haroonamani92@gmail.com>
2025-01-25 21:00:03 +0300

    minor modification

--- app/src/main/java/com/example/restaurantreview/MainActivity.java
+++ app/src/main/java/com/example/restaurantreview/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.restaurantreview;
 
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +9,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle drawerToggle;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,11 +43,46 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout.addDrawerListener(drawerToggle);
         drawerToggle.syncState();
 
+        // Initialize NavigationView
+        navigationView = findViewById(R.id.nav_view);
+        setupNavigationView();
+
+        // Set the default fragment (HomeFragment) when the app starts
+        if (savedInstanceState == null) {
+            loadFragment(new HomeFragment());
+            navigationView.setCheckedItem(R.id.nav_home); // Highlight the Home item
+        }
+
         // Adjust padding for system bars
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawer_layout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void setupNavigationView() {
+        navigationView.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+
+            if (id == R.id.nav_home) {
+                loadFragment(new HomeFragment());
+            } else if (id == R.id.nav_review) {
+                loadFragment(new ReviewFragment());
+            } else if (id == R.id.nav_favourite) {
+                loadFragment(new FavouriteFragment());
+            }
+
+            // Close the navigation drawer
+            drawerLayout.closeDrawers();
+            return true;
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.Fragment_layout, fragment); // Replace the current fragment
+        transaction.commit();
     }
 }
